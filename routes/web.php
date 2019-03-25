6d773f27f10e4696d2c9f60d7fdb7b28440d0fd2
heval22 <hb248@njit.edu>
2019-03-25 11:09:18 -0400

    Fix server 500 error

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,4 @@ Route::get('/contact', 'PagesController@contact')->name('contact');
 Route::post('/contact', 'PagesController@store')->name('contact.store');
 Route::get('/thankyou/{name}', 'PagesController@thankyou')->name('thankyou');
 
+
