bb08c9c42d51f93d4282cbcf7ccbdcd575dfa9b5
heval22 <hb248@njit.edu>
2019-03-25 18:51:42 -0400

    Tasks: Update to thank you page. Updated line 3 (3 business days to 5 business days)

--- resources/views/pages/thankyou.blade.php
+++ resources/views/pages/thankyou.blade.php
@@ -3,7 +3,7 @@
     <div class="starter-template">
         <h1>Thank you {{$name}}!</h1>
         <h1>Your inquiry is important to us.</h1>
-        <h1>We will get back to you within 3 business days.</h1>
+        <h1>We will get back to you within 5 business days.</h1>
 
     </div>
 @stop
